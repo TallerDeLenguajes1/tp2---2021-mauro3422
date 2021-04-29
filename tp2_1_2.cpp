c9797fc461e14ec478ff74da3ae6e5cee9073ffd
Mauro Ramirez <mauroamaya58@gmail.com>
2021-04-29 14:02:54 -0300

    Modificacion del archivo.

--- tp2_1_2.cpp
+++ tp2_1_2.cpp
@@ -7,7 +7,7 @@ int main(void){
 int f,c;
 double mt[N][M];
 
-
+//Declaro un puntero
 double *p;
 p=&mt[0][0];
 
@@ -16,11 +16,11 @@ for(f = 0;f<N; f++)
 {
     for(c = 0;c<M; c++)
     {
+        //Uso Aritmetica de punteros.
         printf("%lf\t",*(p+f*M+c));
     }
     printf("\n");
 }
-
 getchar();
 return 0;
 }
