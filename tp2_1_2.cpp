8127d7701aaccc60ee38fd000f2ca9bd731b4498
Mauro Ramirez <mauroamaya58@gmail.com>
2021-04-29 13:55:20 -0300

    Creacion del archivo tp2_1_2.cpp

---
+++ tp2_1_2.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#define N 4
+#define M 5
+
+
+int main(void){
+int f,c;
+double mt[N][M];
+
+
+double *p;
+p=&mt[0][0];
+
+
+for(f = 0;f<N; f++)
+{
+    for(c = 0;c<M; c++)
+    {
+        printf("%lf\t",*(p+f*M+c));
+    }
+    printf("\n");
+}
+
+getchar();
+return 0;
+}
